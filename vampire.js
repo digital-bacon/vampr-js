24ad278d75e41024a5f144014efd35d25d87d095
Mark Bieganek <mark.bieganek@telus.com>
2022-12-02 18:29:54 -0700

    Add logic to 3/5 class methods

--- vampire.js
+++ vampire.js
@@ -10,22 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    const totalOffspring = this.offspring.length;
+    return totalOffspring;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let thisVampire = this;
 
+    while (thisVampire.creator) {
+      thisVampire = thisVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    
   }
 
   /** Stretch **/
