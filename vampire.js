99c7a3a5bf49aa376efb9d6363fee4cb7906e58a
Mark Bieganek <mark.bieganek@telus.com>
2022-12-02 20:42:23 -0700

    Add method for depthFirstTraversal()

--- vampire.js
+++ vampire.js
@@ -63,7 +63,7 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {   
+  closestCommonAncestor(vampire) {
     
     let thisVampire = this;
     while (thisVampire.isAncestor(vampire) === false) {
@@ -75,7 +75,45 @@ class Vampire {
     return thisVampire;
     
   }
+
+  depthFirstTraversal() {
+    console.log(this.name); // 1
+    for (const offspring of this.offspring) {
+      offspring.depthFirstTraversal(); // 2
+    }
+  }
 }
 
-module.exports = Vampire;
+const rootVampire = new Vampire("root");
+const offspring1 = new Vampire("a");
+const offspring2 = new Vampire("b");
+const offspring3 = new Vampire("c");
+const offspring4 = new Vampire("d");
+const offspring5 = new Vampire("e");
+const offspring6 = new Vampire("f");
+const offspring7 = new Vampire("g");
+const offspring8 = new Vampire("h");
 
+rootVampire.addOffspring(offspring1);
+rootVampire.addOffspring(offspring2);
+rootVampire.addOffspring(offspring3);
+offspring3.addOffspring(offspring4);
+offspring3.addOffspring(offspring5);
+offspring5.addOffspring(offspring6);
+offspring6.addOffspring(offspring7);
+offspring2.addOffspring(offspring8);
+
+console.log(rootVampire.depthFirstTraversal())
+
+// Data structure outline
+/*
+      root
+    /   |   \
+  [a]  [b]  [c]
+        |   / \
+       [h][d] [e]
+               |
+              [g]
+*/
+
+module.exports = Vampire;
